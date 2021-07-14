73a49b871d561eaed15a4aa9dbb9beb827edea4f
Reubber Sousa <reubbers@alu.ufc.br>
2021-07-14 12:23:09 -0300

    add SavePurchase use case

---
+++ clean-cache-control/src/domain/usecases/save-purchases.ts
@@ -0,0 +1,12 @@
+export interface SavePurchases {
+  save: (purchases: Array<SavePurchases.Params>) => Promise<void>
+}
+
+namespace SavePurchases {
+  export type Params = {
+    id :string
+    date: Date
+    value: number
+  } 
+}
+
